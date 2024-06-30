dc2736c735cf3c321c6c52aeec5552c396e53a76
Alexandre Kozoubsky <akozoubsky@gmail.com>
2024-06-30 11:35:39 -0300

    Botão form comentário

---
+++ page-templates/page-no-sidebar.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Página Sem Barra Lateral V.1
+ * 
+ * The template for displaying all pages without sidebar
+ * 
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package minimalista
+ */
+
+get_header();
+?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+			<main id="primary" class="site-main">
+
+				<?php
+				while ( have_posts() ) :
+					the_post();
+
+					get_template_part( 'template-parts/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+
+				endwhile; // End of the loop.
+				?>
+
+			</main><!-- #main -->
+		</div>
+		
+	</div>
+</div>
+
+<?php
+get_footer();
+?>
