d01637978d4838f3ce7d24381779d72a02c2d075
Alexandre Kozoubsky <akozoubsky@gmail.com>
2024-07-10 00:23:42 -0300

    404

--- 404.php
+++ 404.php
@@ -59,9 +59,9 @@ get_header();
 							/* translators: %1$s: smiley */
 							$minimalista_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'minimalista' ), convert_smilies( ':)' ) ) . '</p>';
 							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$minimalista_archive_content" );
-
-							the_widget( 'WP_Widget_Tag_Cloud' );
 							?>
+
+							<?php the_widget( 'WP_Widget_Tag_Cloud' );?>
 
 					</div><!-- .page-content -->
 
