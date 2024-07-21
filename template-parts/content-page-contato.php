e0a38d5ac5abd9b82a9d24a09f320c382b3a9469
Alexandre Kozoubsky <akozoubsky@gmail.com>
2024-07-20 22:27:18 -0300

    minimalista_display_edit_post_link()

--- template-parts/content-page-contato.php
+++ template-parts/content-page-contato.php
@@ -23,26 +23,6 @@
 	minimalista_link_pages();
 	?>
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'minimalista' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+	<?php minimalista_display_edit_post_link(); ?>	
+
 </article><!-- #post-<?php the_ID(); ?> -->
