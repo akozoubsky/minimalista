5a0ae9853739eba763ec1345c4f12fd07770ffca
Alexandre Kozoubsky <akozoubsky@gmail.com>
2024-07-20 23:44:26 -0300

    Functions

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -68,3 +68,226 @@ function get_featured_image_format($post_id = null)
         return 'square';
     }
 }
+
+/* ########################################################
+ *                    Seguranca
+ * ######################################################## */
+
+/**
+ * Change the base for author URLs and flush the rewrite rules if necessary.
+ *
+ * This function changes the base for author URLs from 'author' to 'perfil' and flushes the
+ * rewrite rules to apply the changes if they are not already configured correctly.
+ *
+ * @global WP_Rewrite $wp_rewrite WordPress rewrite rules global.
+ */
+function minimalista_change_author_base() {
+    global $wp_rewrite;
+    $author_base = 'perfil';
+
+    if ( $wp_rewrite->author_base !== $author_base ) {
+        $wp_rewrite->author_base = $author_base;
+        $wp_rewrite->flush_rules();
+    }
+}
+add_action( 'init', 'minimalista_change_author_base' );
+
+/**
+ * Add disallow rules to the robots.txt file to prevent indexing of specified paths or file types.
+ *
+ * This function creates or updates the robots.txt file to include disallow rules for specified
+ * paths or file types. It merges the provided array of paths with a default set of disallowed paths.
+ *
+ * @param array $disallowed_paths Array of paths or file types to disallow in the robots.txt file.
+ */
+function minimalista_add_robots_rule( $disallowed_paths = array() ) {
+    // Ensure $disallowed_paths is always an array
+    if ( !is_array( $disallowed_paths ) ) {
+        $disallowed_paths = array();
+    }
+
+    // Caminhos padrão a serem desindexados
+    $default_disallowed_paths = array(
+        '/wp-admin/',
+        '/wp-admin/admin-ajax.php',
+        '/wp-includes/',
+        '/wp-content/plugins/',
+        '/wp-content/themes/',        
+        '/wp-content/uploads',
+        '/wp-content/files',
+        '/wp-content/cache',
+        '/trackback',
+        '/wp-login.php',
+        '/wp-signup.php',
+        '/readme.html',
+        '/license.txt',
+        '/wp-config-sample.php',
+        '/cgi-bin/',
+        '/wp-content/cache/',
+        '/wp-content/debug.log',
+        '/search/',
+        '/feed/',
+        '/comments/feed/',
+        '/wp-json/',
+        '/author/',
+        '/autor/',
+        '/perfil/',
+        '/writer/',
+        '/*.php$',
+        '/*.inc$',
+        '/*.gz$',
+        '/*.log$',
+        '/*.sql$',
+        '/*.tar$',
+        '/*.zip$',
+        '/*readme.txt$',
+        '/*README.txt$',
+        '/*readme.md$',
+        '/*README.md$',
+        '/*favicon.*$' // Matches any file with "favicon" in its name and any extension
+    );
+
+    // Merge arrays
+    $disallowed_paths = array_merge( $default_disallowed_paths, $disallowed_paths );
+
+    // Verifica se o arquivo robots.txt existe
+    if ( ! file_exists( ABSPATH . 'robots.txt' ) ) {
+        // Cria o arquivo robots.txt se não existir
+        $robots_file = fopen( ABSPATH . 'robots.txt', 'w' );
+        $robots_content = "User-agent: *\n";
+        foreach ( $disallowed_paths as $path ) {
+            $robots_content .= "Disallow: $path\n";
+        }
+        fwrite( $robots_file, $robots_content );
+        fclose( $robots_file );
+    } else {
+        // Verifica se o conteúdo já está presente no arquivo robots.txt
+        $robots_content = file_get_contents( ABSPATH . 'robots.txt' );
+        foreach ( $disallowed_paths as $path ) {
+            if ( strpos( $robots_content, "Disallow: $path" ) === false ) {
+                $robots_content .= "Disallow: $path\n";
+            }
+        }
+        file_put_contents( ABSPATH . 'robots.txt', $robots_content );
+    }
+}
+//add_action( 'init', 'minimalista_add_robots_rule' );
+
+/**
+ * Chamar a função minimalista_add_robots_rule no tema ou em um plugin e passar um array adicional de caminhos que deseja desindexar.
+ *
+ */
+/* 
+add_action( 'init', function() {
+    $extra_disallowed_paths = array(
+        '/author/',
+        '/perfil/',
+    );
+    minimalista_add_robots_rule( $extra_disallowed_paths );
+});
+*/
+
+/** @TODO A FUNCAO NAO ESTAH COMPLETA. TEM BUG.
+ * Adiciona regras ao arquivo .htaccess para configurar o servidor Apache.
+ *
+ * Esta função cria ou atualiza o arquivo .htaccess para incluir regras especificadas.
+ * Ela mescla o array fornecido de regras com um conjunto padrão de regras.
+ * 
+ * Add disallow rules to the .htaccess file to configure the Apache server.
+ *
+ * This function creates or updates the .htaccess file to include specified rules.
+ * It merges the provided array of rules with a default set of rules.
+ *
+ * @param array $rules Array de regras para adicionar ao arquivo .htaccess.
+ */
+function minimalista_add_htaccess_rules( $rules = array() ) {
+    // Ensure $rules is always an array
+    if ( ! is_array( $rules ) ) {
+        $rules = array();
+    }
+
+    // Default rules to be added to the .htaccess
+    $default_rules = array(
+        "# Bloquear o acesso aos arquivos de configuração do WordPress",
+        "<FilesMatch \"^wp-config.php$\">",
+        "    Order deny,allow",
+        "    Deny from all",
+        "</FilesMatch>",
+        "",
+        "# Bloquear o acesso aos arquivos de log",
+        "<FilesMatch \"\\.log$\">",
+        "    Order deny,allow",
+        "    Deny from all",
+        "</FilesMatch>",
+        "",
+        "# Bloquear o acesso aos arquivos de banco de dados",
+        "<FilesMatch \"\\.(sql|tar|zip|gz)$\">",
+        "    Order deny,allow",
+        "    Deny from all",
+        "</FilesMatch>",
+        "",
+        "# Bloquear o acesso a arquivos readme e markdown",
+        "<FilesMatch \"(?i)readme\\.(txt|md)$\">",
+        "    Order deny,allow",
+        "    Deny from all",
+        "</FilesMatch>",
+        "",
+        "# Bloquear o acesso a arquivos HTML específicos",
+        "<FilesMatch \"readme\\.html$\">",
+        "    Order deny,allow",
+        "    Deny from all",
+        "</FilesMatch>",
+        "",
+        "# Bloquear o acesso a arquivos favicon",
+        "<FilesMatch \"(?i)favicon\\.(ico|png|gif|jpg)$\">",
+        "    Order deny,allow",
+        "    Deny from all",
+        "</FilesMatch>",
+        "",
+    );
+
+    // Merge arrays
+    $rules = array_merge( $default_rules, $rules );
+
+    // Path to the .htaccess file
+    $htaccess_path = ABSPATH . '.htaccess';
+
+    // Check if the .htaccess file exists
+    if ( file_exists( $htaccess_path ) ) {
+        // Read the existing .htaccess content
+        $htaccess_content = file_get_contents( $htaccess_path );
+
+        // Check if each rule already exists before adding
+        foreach ( $rules as $rule ) {
+            $rule = trim( $rule ); // Remove whitespace from the rule
+            if ( ! empty( $rule ) && strpos( $htaccess_content, $rule ) === false ) {
+                // Add the rule only if it doesn't exist
+                $htaccess_content .= "\n" . $rule;
+            }
+        }
+    } else {
+        // Create the .htaccess file with the new rules
+        $htaccess_content = implode( "\n", $rules ) . "\n";
+    }
+
+    // Write the updated content to the .htaccess file
+    file_put_contents( $htaccess_path, $htaccess_content );
+}
+//add_action( 'init', 'minimalista_add_htaccess_rules' );
+
+/**
+ * Chamar a função minimalista_add_htaccess_rules no tema ou em um plugin e passar um array adicional de regras.
+ *
+ */
+/* 
+add_action( 'init', function() {
+    $extra_rules = array(
+        '# Bloquear o acesso ao diretório wp-content/uploads',
+        '<Directory "/wp-content/uploads">',
+        '    Order deny,allow',
+        '    Deny from all',
+        '</Directory>',
+    );
+    minimalista_add_htaccess_rules( $extra_rules );
+});
+*/
