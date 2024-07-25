fd8debe4fb21fb520a9c2af6143b5fc36f1fd621
Alexandre Kozoubsky <akozoubsky@gmail.com>
2024-07-25 13:32:54 -0300

    widget area - no-collapse

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The sidebar containing the main widget area
  *
@@ -7,23 +8,20 @@
  * @package minimalista
  */
 
- // Prevent direct access to the file
+// Prevent direct access to the file
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
 /* Verifica se a área de widget sidebar-1 está ativa. Se não houver widgets ativos na área de widget sidebar-1, o código retorna e não renderiza a barra lateral. */
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
+if (!is_active_sidebar('sidebar-1')) {
+    return;
 }
 ?>
 <!-- <aside id="secondary" class="widget-area col-lg-4 mt-5 mt-lg-0 ps-lg-5"> -->
 <aside id="secondary" class="widget-area col-lg-4 mt-5 mt-lg-0 ps-lg-5">
-<div class="">
-    <div class="">
-        <div class="">
-            <?php dynamic_sidebar( 'sidebar-1' ); ?>
-        </div>
+    <div class="no-collapse-3">
+        <?php dynamic_sidebar('sidebar-1'); ?>
     </div>
-</aside><!-- #secondary -->
 
+</aside><!-- #secondary -->
