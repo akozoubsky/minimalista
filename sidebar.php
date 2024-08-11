b88ecd0b5da3d4a0a13a1548c6e2bff9aad3ff19
Alexandre Kozoubsky <akozoubsky@gmail.com>
2024-08-11 02:02:40 -0300

    div structure

--- sidebar.php
+++ sidebar.php
@@ -14,15 +14,16 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-/* Verifica se a área de widget sidebar-1 está ativa. Se não houver widgets ativos na área de widget sidebar-1, o código retorna e não renderiza a barra lateral. */
 if (!is_active_sidebar('sidebar-1')) {
     return;
 }
 ?>
-<!-- <aside id="secondary" class="widget-area col-lg-4 mt-5 mt-lg-0 ps-lg-5"> -->
-<aside id="secondary" class="col-lg-4">
-    <div class="sidebar widget-area ps-lg-3">
-        <?php dynamic_sidebar('sidebar-1'); ?>
-    </div>
 
-</aside><!-- #secondary -->
+<div class="col-lg-4">
+    <aside id="secondary" class="sidebar-right">
+        <div class="sidebar widget-area ps-lg-3">
+            <?php dynamic_sidebar('sidebar-1'); ?>
+        </div>
+
+    </aside><!-- #secondary -->
+</div><!-- ./col -->
